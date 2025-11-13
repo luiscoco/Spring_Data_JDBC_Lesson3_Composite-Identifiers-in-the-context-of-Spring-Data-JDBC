8c94a18701cf4e4ce84128a89580436f052663b9
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2025-11-13 17:30:41 +0100

    Delete target directory

--- target/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ORDERS (
-    customer_id VARCHAR(50) NOT NULL,
-    order_number BIGINT NOT NULL,
-    order_date DATE,
-    description VARCHAR(255),
-    PRIMARY KEY (customer_id, order_number)
-);
